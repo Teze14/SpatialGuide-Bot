b37457d76d72570356965ab750c9ac11d3bb48fc
Jesús Emiliano García Jiménez <143450321+Teze14@users.noreply.github.com>
2025-12-07 17:11:34 -0600

    Update directory paths in run_mr.sh

--- run_mr.sh
+++ run_mr.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # 1) Activar el entorno virtual donde está Whisper
-cd /home/jesusgj/mr_voz
+cd /home/usr/mr_voz
 source venv/bin/activate
 
 # 2) Ir a la carpeta del Mini-Rover integrado
-cd /home/jesusgj/MR_Integrado
+cd /home/usr/MR_Integrado
 
 # 3) Ejecutar el main con ese Python del venv
 python main_mr.py
